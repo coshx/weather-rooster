2b7d095c50f5477b601827381025ea713d49de9b
Gabe Kopley <gabe@coshx.com>
2012-08-22 11:12:05 -0700

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherRooster::Application.config.secret_token = '6d8737a9b79e7464aed493b47a188fba22d2683bc638982b844eb0ac147d3b844a9a74294895609fd93b2d329640e631a9bc3b781e7790ad525b57d3919d344a'
