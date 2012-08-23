e716119dbb06ef64aae7c5ced74d1f43936ac699
Michael <info1117@gmail.com>
2012-08-23 10:19:17 -0700

    assets configuration

--- config/application.rb
+++ config/application.rb
@@ -51,6 +51,12 @@ module WeatherRooster
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
+    config.assets.enabled = true
+    config.assets.compile = true
+    config.assets.precompile += ['foundation'] 
+
+    config.assets.initialize_on_precompile = false
+    
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
