4198697678180e8f234d6f0ee98d0b0e74242b52
Michael <info1117@gmail.com>
2012-08-23 15:01:41 -0700

    new city detection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+
+  def slug(name)
+    str = "#{name}"
+    str.gsub! /['`]/,""
+    str.gsub! /\s*@\s*/, " at "      
+    str.gsub! /\s*&\s*/, " and "
+    str.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+    str.gsub! /_+/,"_"
+    str.gsub! /\A[_\.]+|[_\.]+\z/,""
+    str.downcase!
+    return str
+  end
+
 end
