a95d879cb9721169234a605141d0813e92a5989b
Michael <info1117@gmail.com>
2012-08-22 21:45:32 -0700

    pg no pry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,7 @@ class HomeController < ApplicationController
   GeoIp.api_key = '6881154858158dd019be66f40bbc3c31e41d6f9fc6bcb411759ca4c2be2ea8c8'
    ip_address = request.remote_ip
    GeoIp.geolocation(ip_address) 
-
-    binding.pry
+    
 
   end
   def thank_you
