d25a9e9ff64f4c6fb7f923c237cd8794533b4a04
Michael <info1117@gmail.com>
2012-08-23 23:46:12 -0700

    frontend and data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,5 +11,20 @@ class HomeController < ApplicationController
     @services.map! {|s| {:service => s,
                                :score => s.recent_cc_score(@my_city) } }
     @services.sort! {|x,y| y[:score] <=> x[:score] }
+    
+    @current_temp = @services.first[:service].current_forecasts.where(:city_id => @my_city.id).first.current_temp   
+    
+    @provider_name  = @services.first[:service].current_forecasts.where(:city_id => @my_city.id).first.weather_service.short_name
+    
+    @day = []
+    
+    4.times do |i|
+      @day[i] = {}
+      @day[i][:high] = @services.first[:service].current_forecasts.where(:city_id => @my_city.id).first["day_#{i}_high"]
+      @day[i][:low] = @services.first[:service].current_forecasts.where(:city_id => @my_city.id).first["day_#{i}_low"]
+
+    end
+    
+
   end
 end
