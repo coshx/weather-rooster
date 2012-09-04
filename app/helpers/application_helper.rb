10155f1725b4f2715f5df84462860b8e3d6a4192
Michael <info1117@gmail.com>
2012-09-03 17:17:33 -0700

    validation of NOAA

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,21 +4,21 @@ module ApplicationHelper
   def slug(name)
     str = "#{name}"
     str.gsub! /['`]/,""
-    str.gsub! /\s*@\s*/, " at "      
+    str.gsub! /\s*@\s*/, " at "
     str.gsub! /\s*&\s*/, " and "
-    str.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+    str.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'
     str.gsub! /_+/,"_"
     str.gsub! /\A[_\.]+|[_\.]+\z/,""
     str.downcase!
     return str
   end
-  
-  
+
+
   def translate_status(api_name)
     #status = Weather_status.find_by_api_name(api_name)
     #return status.name
   end
-  
+
   #Time.current
   def weather_record_valid?(the_time)
     true if (Time.current - the_time) < 36.hours
@@ -27,5 +27,10 @@ module ApplicationHelper
   def current_forecast_valid?(the_time)
     true if (Time.current - the_time) < 20.minutes
   end
+
+  def noaa_weather_record_valid?(the_time)
+    true if (Time.current - the_time) < 100.hours
+  end
+
 
 end
